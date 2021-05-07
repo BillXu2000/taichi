7ed52a5a47a04922fe629ed4412c605696bbdc8d
bx2k <BillXu2000@126.com>
2021-05-07 16:46:29 +0800

    compile hello world for new demo

--- cmake/FrontendDemo.cmake
+++ cmake/FrontendDemo.cmake
@@ -1,13 +1,17 @@
 cmake_minimum_required(VERSION 3.0)
 
-set(TESTS_NAME frontend_demo)
-set(DEMO_LIB_NAME demo)
+set(PLOTTER_DEMO_NAME plotter_demo)
+set(CELL_DEMO_NAME cell_demo)
 
-file(GLOB_RECURSE FRONTEND_DEMO_SOURCE "tests/plotter_demo/*.cpp")
+file(GLOB_RECURSE PLOTTER_DEMO_SOURCE "tests/plotter_demo/*.cpp")
+file(GLOB_RECURSE CELL_DEMO_SOURCE "tests/cell_demo/*.cpp")
 
 include_directories(
     ${PROJECT_SOURCE_DIR},
 )
 
-add_executable(${TESTS_NAME} ${FRONTEND_DEMO_SOURCE})
-target_link_libraries(${TESTS_NAME} taichi_isolated_core)
+add_executable(${PLOTTER_DEMO_NAME} ${PLOTTER_DEMO_SOURCE})
+target_link_libraries(${PLOTTER_DEMO_NAME} taichi_isolated_core)
+
+add_executable(${CELL_DEMO_NAME} ${CELL_DEMO_SOURCE})
+target_link_libraries(${CELL_DEMO_NAME} taichi_isolated_core)
