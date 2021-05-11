cc3adb648a76a72c5c684f58f0919916b60445b7
bx2k <BillXu2000@126.com>
2021-05-11 11:28:49 +0800

    game of life runnable

--- tests/cell_demo/main.cpp
+++ tests/cell_demo/main.cpp
@@ -14,6 +14,66 @@
 
 #include "taichi/gui/gui.h"
 
+class BuilderHelper {
+public:
+    static taichi::lang::IRBuilder *builder;
+    taichi::lang::Stmt *stmt;
+
+    BuilderHelper(taichi::lang::Stmt *_stmt): stmt(_stmt) {}
+
+    BuilderHelper operator + (taichi::lang::Stmt *other) {
+        return builder->create_add(stmt, other);
+    }
+
+    BuilderHelper operator - (taichi::lang::Stmt *other) {
+        return builder->create_sub(stmt, other);
+    }
+
+    BuilderHelper operator * (taichi::lang::Stmt *other) {
+        return builder->create_mul(stmt, other);
+    }
+
+    BuilderHelper operator / (taichi::lang::Stmt *other) {
+        return builder->create_div(stmt, other);
+    }
+
+    BuilderHelper operator % (taichi::lang::Stmt *other) {
+        return builder->create_mod(stmt, other);
+    }
+
+    BuilderHelper operator > (taichi::lang::Stmt *other) {
+        return builder->create_cmp_gt(stmt, other);
+    }
+
+    BuilderHelper operator | (taichi::lang::Stmt *other) {
+        return builder->create_or(stmt, other);
+    }
+
+    BuilderHelper operator == (taichi::lang::Stmt *other) {
+        return builder->create_cmp_eq(stmt, other);
+    }
+
+    BuilderHelper operator & (taichi::lang::Stmt *other) {
+        return builder->create_and(stmt, other);
+    }
+
+    operator taichi::lang::Stmt*() {
+        return stmt;
+    }
+};
+taichi::lang::IRBuilder *BuilderHelper::builder(nullptr);
+
+class BuilderHelperGuard {
+public:
+    BuilderHelperGuard(taichi::lang::IRBuilder &builder) {
+        BuilderHelper::builder = &builder;
+    }
+
+    ~BuilderHelperGuard() {
+        BuilderHelper::builder = nullptr;
+    }
+};
+
 void game_of_life() {
     using namespace std;
     using namespace taichi;
@@ -23,17 +83,22 @@ void game_of_life() {
     config_print_ir.print_ir = true;
     program.config = config_print_ir;  // print ir
     auto root = program.snode_root.get();
-    auto &dense = root->dense(Index(0), 10);
-    auto &alive = dense.insert_children(SNodeType::place);
+
+    const int N = 512;
+    std::vector<Index> index_dense = {0, 1};
+    std::vector<int> size_dense = {N, N};
+    auto &alive = root->dense(index_dense, size_dense).insert_children(SNodeType::place);
     alive.dt = PrimitiveType::i32;
-    auto &next = dense.insert_children(SNodeType::place);
+    auto &next = root->dense(index_dense, size_dense).insert_children(SNodeType::place);
     next.dt = PrimitiveType::i32;
     program.materialize_layout();
 
-    const int N = 512;
-    std::unique_ptr<Kernel> step_kernel;
+    typedef BuilderHelper BH;
+
+    std::unique_ptr<Kernel> kernel_init;
     {
         IRBuilder builder;
+        BuilderHelperGuard _(builder);
         auto *left = builder.get_int32(1);
         auto *right = builder.get_int32(N - 1);
         auto *loop_x = builder.create_range_for(left, right, 1, 0, 4);
@@ -44,7 +109,31 @@ void game_of_life() {
             {
                 auto _ = builder.get_loop_guard(loop_y);
                 auto *y = builder.get_loop_index(loop_y, 0);
-                Stmt *sum = nullptr, *sum_ex = nullptr;
+                Stmt *cnt = BH(x) * builder.get_int32(N) + y;
+                auto *mod = builder.get_int32(10007);
+                Stmt *ans = (BH(cnt) * builder.get_int32(9287) % mod + mod) % builder.get_int32(2);
+                vector<Stmt *> indices = {x, y};
+                builder.create_global_store(builder.create_global_ptr(&alive, indices), ans);
+            }
+        }
+        kernel_init = make_unique<Kernel>(program, builder.extract_ir());
+    }
+    
+    std::unique_ptr<Kernel> kernel_step;
+    {
+        IRBuilder builder;
+        BuilderHelperGuard _(builder);
+        auto *left = builder.get_int32(1);
+        auto *right = builder.get_int32(N - 1);
+        auto *loop_x = builder.create_range_for(left, right, 1, 0, 4);
+        {
+            auto _ = builder.get_loop_guard(loop_x);
+            auto *x = builder.get_loop_index(loop_x, 0);
+            auto *loop_y = builder.create_range_for(left, right, 1, 0, 4);
+            {
+                auto _ = builder.get_loop_guard(loop_y);
+                auto *y = builder.get_loop_index(loop_y, 0);
+                Stmt *sum = nullptr, *self = nullptr;
                 for (int i = -1; i < 2; i++) {
                     for (int j = -1; j < 2; j++) {
                         vector<Stmt *> indices = {
@@ -53,22 +142,21 @@ void game_of_life() {
                         auto *load = builder.create_global_load(builder.create_global_ptr(&alive, indices));
                         if (sum) sum = builder.create_add(sum, load);
                         else sum = load;
-                        if (!i && !j) sum_ex = builder.create_sub(sum, load);
+                        if (!i && !j) self = load;
                     }
                 }
-                auto ans = builder.create_or(
-                    builder.create_cmp_eq(sum, builder.get_int32(3)),
-                    builder.create_cmp_eq(sum_ex, builder.get_int32(3)));
+                auto ans = (BH(sum) == builder.get_int32(3) | (BH(sum) - self) == builder.get_int32(3)) & builder.get_int32(1);
                 vector<Stmt *> indices = {x, y};
                 builder.create_global_store(builder.create_global_ptr(&next, indices), ans);
             }
         }
-        step_kernel = make_unique<Kernel>(program, builder.extract_ir());
+        kernel_step = make_unique<Kernel>(program, builder.extract_ir());
     }
 
-    std::unique_ptr<Kernel> swap_kernel;
+    std::unique_ptr<Kernel> kernel_swap;
     {
         IRBuilder builder;
+        BuilderHelperGuard _(builder);
         auto *left = builder.get_int32(1);
         auto *right = builder.get_int32(N - 1);
         auto *loop_x = builder.create_range_for(left, right, 1, 0, 4);
@@ -86,7 +174,60 @@ void game_of_life() {
                         builder.create_global_ptr(&next, indices)));
             }
         }
-        swap_kernel = make_unique<Kernel>(program, builder.extract_ir());
+        kernel_swap = make_unique<Kernel>(program, builder.extract_ir());
+    }
+
+    std::unique_ptr<Kernel> kernel_gui;
+    {
+        IRBuilder builder;
+        BuilderHelperGuard _(builder);
+        auto *left = builder.get_int32(1);
+        auto *right = builder.get_int32(N - 1);
+        auto *loop_x = builder.create_range_for(left, right, 1, 0, 4);
+        {
+            auto _ = builder.get_loop_guard(loop_x);
+            auto *x = builder.get_loop_index(loop_x, 0);
+            auto *loop_y = builder.create_range_for(left, right, 1, 0, 4);
+            {
+                auto _ = builder.get_loop_guard(loop_y);
+                auto *y = builder.get_loop_index(loop_y, 0);
+                vector<Stmt *> indices = {x, y};
+                builder.create_global_store(
+                    builder.create_external_ptr(
+                        builder.create_arg_load(0, PrimitiveType::i32, true),
+                        vector<Stmt *>(1, BH(x) * builder.get_int32(N) + y)),
+                    builder.create_global_load(
+                        builder.create_global_ptr(&alive, indices)));
+            }
+        }
+        kernel_gui = make_unique<Kernel>(program, builder.extract_ir());
+        kernel_gui->insert_arg(PrimitiveType::gen, true);
+    }
+    auto gui = GUI("cell language", N, N, true, false, 0, false, false);
+    auto &canvas = *gui.canvas;
+    static int ans[N][N];
+    auto ctx_init = kernel_init->make_launch_context();
+    auto ctx_step = kernel_step->make_launch_context();
+    auto ctx_swap = kernel_swap->make_launch_context();
+    auto ctx_gui = kernel_gui->make_launch_context();
+    ctx_gui.set_arg_external_array(0, taichi::uint64(ans), 0);
+    (*kernel_init)(ctx_init);
+    for (int frame = 0;; frame++) {
+        gui.update();
+        if (frame % 60) continue;
+        (*kernel_step)(ctx_step);
+        if (frame > 60) (*kernel_swap)(ctx_swap);
+        (*kernel_gui)(ctx_gui);
+        long long sum = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                float k = ans[i][j];
+                sum += ans[i][j];
+                std::array<taichi::real, 4> color{k, 0, 0, 1};
+                canvas.img[i][j] = taichi::Vector4(color);
+            }
+        }
+        cerr << sum << ": sum\n";
     }
 }
 
